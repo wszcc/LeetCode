ca81e03b88c7906bd5d632e265a40337d38f79b2
FY1010 <1114550440@qq.com>
2021-01-29 21:50:38 +0800

    codebox done

--- src/utils/shared.tsx
+++ src/utils/shared.tsx
@@ -9,7 +9,7 @@ export const storage = {
   get(key: string) {
     return localStorage.getItem(key)
   },
-  set(key: string, obj: string | object) {
+  set(key: string, obj: string | number | object) {
     if (!obj) {
       console.warn("存入失败，存入的不是对象或字符串类型")
       console.warn(obj);
@@ -18,7 +18,7 @@ export const storage = {
     if (typeof obj === 'object') {
       obj = JSON.stringify(obj)
     }
-    localStorage.setItem(key, obj)
+    localStorage.setItem(key, obj + "")
   },
   clear() {
     localStorage.clear()
@@ -34,5 +34,25 @@ export function forEach(target: LooseObj, callback: (...args: any) => any) {
   }
   for (let i = 0; i < target.length; i++) {
     callback(target[i])
+  }
+}
+
+export const Int = (str: string): number => {
+  return parseInt(str)
+}
+
+export const Float = (str: string): number => {
+  return parseFloat(str)
+}
+
+export function debounce<T extends any[]>(fn: (...args: T) => any, ms: number = 1000) {
+  let timer: number | null = null
+  return (...args: T) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = window.setTimeout(() => {
+      fn(...args)
+    }, ms)
   }
 }
