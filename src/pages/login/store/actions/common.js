8cf644305fa2ff221dc42fc87aabaf38e30f0881
Yukee-798 <909397116@qq.com>
2021-03-05 16:40:39 +0800

    update register

--- src/pages/login/store/actions/common.js
+++
@@ -1,7 +0,0 @@
-import {
-        NETWORK_ERROR,
-        NETWORK_NORMAL
-} from '../constant'
-
-export const networkError = data => ({type: NETWORK_ERROR, data});
-export const newworkNormal = data => ({type: NETWORK_NORMAL, data});
