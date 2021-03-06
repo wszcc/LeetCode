d0676169dc868d85e53c2fd14334583f63cf698f
Yukee-798 <909397116@qq.com>
2021-03-06 16:00:06 +0800

    update login

--- src/pages/login/components/LoginMain/ErrorInfo/errorInfo.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const InvalidPhoneNumber: React.FC = () => <div className='error-info'>请输入正确的手机号</div>;
-export const EmptyPhoneNumber: React.FC = () => <div className='error-info'>请输入手机号</div>;
-export const EmptyCaptcha: React.FC = () => <div className='error-info'>请输入验证码</div>;
-export const EmptyUsername: React.FC = () => <div className='error-info'>请输入手机号或邮箱</div>;
-export const EmptyPassword: React.FC = () => <div className='error-info'>请输入密码</div>;
